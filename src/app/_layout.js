5b4cc814b6d23d5ab65e5d14e37090eadccb6f61
karolesss <karolinegit@gmail.com>
2024-09-10 12:56:42 +0000

    [feat] Auth Context

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,11 +1,39 @@
-import { Stack } from "expo-router";
+import { Stack, useSegments, router } from "expo-router";
 import { AppProvider } from "../hooks";
-import { Text } from "react-native";
+import { useEffect } from "react";
+import { useAuth } from "../hooks/Auth";
+
+const StackLayout = () => {
+
+  const { user } = useAuth();
+  const segments = useSegments();
+
+  useEffect(() => {
+    const inAuthGroup = segments[0] === "(protected)";
+
+    if(!user?.autenticated && inAuthGroup) {
+      router.replace("/")
+    } else {
+      if(user?.autenticated){
+        router.replace("/(protected)");
+      }
+    }
+  }, [user]);
+  
+  return (
+    <Stack>
+    <Stack.Screen name="index" />
+    <Stack.Screen name="(protected)" />
+    </Stack>
+  );
+}
 
 export default function Layout() {
+
+
   return (
     <AppProvider>
-      <Stack />
+      <StackLayout/>
     </AppProvider>
   );
 }
